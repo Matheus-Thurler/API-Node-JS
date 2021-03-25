8d857dd8d7c9fbf588bfadb6bfc39fdd3effef11
Matheus Thurler <matheusthurlernf@gmail.com>
2021-03-25 19:42:43 -0300

    ajuste de rota

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const router = express.Router()
-// const authMiddleware = require('../src/middlewares/jwt')
+
+// IMPORTE PARA USO DO MIDDLEWARE
+// const authMiddleware = require('../src/middlewares/jwt') 
 
 const userControler = require('../src/controllers/UserController')
-// const videoControler = require('../src/controllers/VideoController')
 
 router.get('/', (req, res) => {
     return res.send("bem vindo")
 })
 
+// EXEMPLO DE ROTA COM MIDDLEWARE, ROTA SOMENTE SE TIVER O TOKEN DO USUARIO
+
+// router.post('/users', authMiddleware, userControler.store)
+
 router.get('/users', userControler.index)
 router.post('/users', userControler.store)
 router.post('/login', userControler.login)
 
-// router.post('/video', videoControler.store)
 
 module.exports = router;
